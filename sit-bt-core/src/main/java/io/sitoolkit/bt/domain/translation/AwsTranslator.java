e454fcd14d9473186a524d03451c1ee19f1159f8
ryohei.komatsu <ryohei.komatsu@monocrea.co.jp>
2022-11-28 18:33:59 +0900

    bug/add-text-limit-to-aws-translate-api

--- sit-bt-core/src/main/java/io/sitoolkit/bt/domain/translation/AwsTranslator.java
+++ sit-bt-core/src/main/java/io/sitoolkit/bt/domain/translation/AwsTranslator.java
@@ -7,6 +7,10 @@ import com.amazonaws.services.translate.AmazonTranslate;
 import com.amazonaws.services.translate.AmazonTranslateClient;
 import com.amazonaws.services.translate.model.TranslateTextRequest;
 import com.amazonaws.services.translate.model.TranslateTextResult;
+import java.text.BreakIterator;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -32,13 +36,50 @@ public class AwsTranslator implements Translator {
 
   private String translate(String text, String sourceLanguageCode, String targetLanguageCode) {
 
-    TranslateTextRequest request =
-        new TranslateTextRequest()
-            .withText(text)
-            .withSourceLanguageCode(sourceLanguageCode)
-            .withTargetLanguageCode(targetLanguageCode);
-    TranslateTextResult result = translate.translateText(request);
+    // 合計ドキュメントサイズを Amazon Translate のドキュメントサイズの制限未満に維持する
+    List<String> sentences = splitSegment(text, getLocaleBySourceLanguageCode(sourceLanguageCode));
+    List<TranslateTextRequest> requests = new ArrayList<>();
+    sentences.stream()
+        .map(
+            sentence ->
+                new TranslateTextRequest()
+                    .withText(sentence)
+                    .withSourceLanguageCode(sourceLanguageCode)
+                    .withTargetLanguageCode(targetLanguageCode))
+        .forEachOrdered(requests::add);
 
-    return result.getTranslatedText();
+    List<TranslateTextResult> results = new ArrayList<>();
+    requests.stream().map(translate::translateText).forEachOrdered(results::add);
+
+    StringBuilder translatedText = new StringBuilder();
+    results.forEach(result -> translatedText.append(result.getTranslatedText()));
+
+    return translatedText.toString();
+  }
+
+  private Locale getLocaleBySourceLanguageCode(String sourceLanguageCode) {
+    switch (sourceLanguageCode) {
+      case "ja":
+        return Locale.JAPAN;
+      case "en":
+        return Locale.ENGLISH;
+      default:
+        return Locale.ENGLISH;
+    }
+  }
+
+  private List<String> splitSegment(String text, Locale locale) {
+    List<String> res = new ArrayList<>();
+    BreakIterator sentenceIterator = BreakIterator.getSentenceInstance(locale);
+    sentenceIterator.setText(text);
+    int prevBoundary = sentenceIterator.first();
+    int curBoundary = sentenceIterator.next();
+    while (curBoundary != BreakIterator.DONE) {
+      String sentence = text.substring(prevBoundary, curBoundary);
+      res.add(sentence);
+      prevBoundary = curBoundary;
+      curBoundary = sentenceIterator.next();
+    }
+    return res;
   }
 }
